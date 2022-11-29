020603968634f37fc77387ec9b6daea5012008e4
hoidiep <110796208+hoidiep@users.noreply.github.com>
2022-11-29 18:14:53 +0700

    loginSQL

--- MusikPlayer8/app/src/main/java/com/freesongs/musikplayer8/MainActivity.java
+++ MusikPlayer8/app/src/main/java/com/freesongs/musikplayer8/MainActivity.java
@@ -3,12 +3,39 @@ package com.freesongs.musikplayer8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password, repassword;
+    Button signup, signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.repassword);
+        signup = (Button) findViewById(R.id.btnsignup);
+        signin = (Button) findViewById(R.id.btnsignin);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
     }
 }
