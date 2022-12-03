81bd3e00989e5db22e0237a0752c6aed0467225c
michng9999 <107618402+michng9999@users.noreply.github.com>
2022-12-03 08:51:49 +0700

    init server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
-// const songRoutes = require("./routes/songs");
+const songRoutes = require("./routes/baihats");
 // const playListRoutes = require("./routes/playLists");
 // const searchRoutes = require("./routes/search");
 const app = express();
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use("/api/users/", userRoutes);
 app.use("/api/login/", authRoutes);
-// app.use("/api/songs/", songRoutes);
+app.use("/api/baihats/", songRoutes);
 // app.use("/api/playlists/", playListRoutes);
 // app.use("/api/", searchRoutes);
 
