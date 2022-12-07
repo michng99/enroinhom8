df71962644652c40eeb94c2eba0c695732b81469
michng9999 <107618402+michng9999@users.noreply.github.com>
2022-12-07 22:58:08 +0700

    init

--- MusikPlayer8/app/src/main/java/com/freesongs/musikplayer8/Playlist/FragmentPlaylist.java
+++ MusikPlayer8/app/src/main/java/com/freesongs/musikplayer8/Playlist/FragmentPlaylist.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
 import com.freesongs.musikplayer8.R;
 
 public class FragmentPlaylist extends Fragment {
@@ -14,6 +16,14 @@ public class FragmentPlaylist extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate( R.layout.fragment_playlist , container , false );
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+
+
+
+
+
+
         return view;
     }
 }
